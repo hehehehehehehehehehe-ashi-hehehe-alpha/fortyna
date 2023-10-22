146179dee2e3ab6340195ed1a3ccd0a3e6e48ce8
hehehehehehehehehehe-ashi-hehehe-alpha <108414168+hehehehehehehehehehe-ashi-hehehe-alpha@users.noreply.github.com>
2023-10-22 11:57:49 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+const downloadLink = document.querySelector('#download-link');
+
+async function downloadTikTokVideo(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  const videoUrl = json.video.download_url;
+
+  const anchor = document.createElement('a');
+  anchor.href = videoUrl;
+  anchor.download = 'tiktok.mp4';
+  downloadLink.appendChild(anchor);
+}
+
+button.addEventListener('click', () => {
+  const url = input.value;
+  downloadTikTokVideo(url);
+});​
+￼Enter
